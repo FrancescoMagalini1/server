15b8280c4331303fa9924d779c0c7aade8b233d3
Francesco <francesco.magalini@unitn.it>
2024-04-08 08:20:22 +0200

    Testing environment variables

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 
+class Settings(BaseSettings):
+    DB_IP: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+
+
+settings = Settings()
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+async def read_root():
+    return {"Hello": "World"}
+
+@app.get("/v1/auth")
+async def auth():
+    return settings.DB_IP
